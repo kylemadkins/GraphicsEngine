129a8dea90ec8513a06508eb1a7f53c6eb2eda8d
Kyle Adkins <kyle.m.adkins@gmail.com>
2025-05-10 18:34:50 -0700

    Remove macOS window hint for OpenGL

--- src/Main.cpp
+++ src/Main.cpp
@@ -37,9 +37,6 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
 
     GLFWwindow* window = glfwCreateWindow(width, height, "Graphics Engine", nullptr, nullptr);
     if (!window) {
