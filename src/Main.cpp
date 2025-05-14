58dd91bc960f374a4c3b475d5a0f7c8227af721c
Kyle Adkins <kyle.m.adkins@gmail.com>
2025-05-14 15:39:43 -0400

    Bind VAO first, VAO remembers VBO binding

--- src/Main.cpp
+++ src/Main.cpp
@@ -85,9 +85,12 @@ int main()
 	GLuint vao, vbo;
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
+
+	glBindVertexArray(vao);
+
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
-	glBindVertexArray(vao);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
